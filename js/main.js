67a7c5e06c9bd8772791f9772985e6628a5395ba
Parisa Shojaei <parisa.shojaei85@gmail.com>
2017-06-30 22:00:18 +0430

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+function showPyramid() {
+    var row = Number(document.getElementById("getRow").value);
+    
+    document.getElementById("pyramid").innerHTML = "";
+    for (var i = 1; i <= row; i++) {
+        var output = "";
+        for (var j = 1; j <= row - i; j++) {
+            output += " ";
+        }
+        for (var k = 1; k <= i; k++) {
+            output += "\u2605 ";
+        }
+        document.getElementById("pyramid").innerHTML += output+"<br>";
+    }
+
+    document.getElementById("invPyramid").innerHTML ="";
+    for (var i = row; i >= 1; i--) {
+        var output = "";
+        for (var j = 1; j <= row - i; j++) {
+            output += " ";
+        }
+        for (var k = 1; k <= i; k++) {
+            output += "\u2605 ";
+        }
+        document.getElementById("invPyramid").innerHTML += output+"<br>";
+    }
+
+    document.getElementById("rPyramid").innerHTML = "";
+    document.getElementById("lPyramid").innerHTML = "";
+    
+    for (var i = 1; i <= row; i++) {
+        var output = "";
+        for (var j = 1; j <= i; j++) {
+            output += "\u2605 ";
+        }
+        document.getElementById("rPyramid").innerHTML += output+"<br>";
+        document.getElementById("lPyramid").innerHTML += output+"<br>";
+    }
+    
+     document.getElementById("showPyramid").style.display = "block";
+     document.getElementById("getRow").value = "";
+}
+
+
 
